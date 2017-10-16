08b5d9f094b085d0fdac3596461e086f33d8e5d9
Hayden <webb.hayden98@gmail.com>
2017-10-16 17:27:40 -0500

    worked on cards

--- Cards.py
+++ Cards.py
@@ -4,8 +4,8 @@ while True:
 
   if Begin == 'Y' or Begin == 'y' or Begin == 'yes' or Begin == 'Yes':
     print 'Excelent, here is your hand sir.'
-    hand1 = random.randint (1,11)
-    hand2 = random.randint (1,11)
+    hand1 = random.randint (2,11)
+    hand2 = random.randint (2,11)
     hvalue = hand2 + hand1
     print 'Your hand value is: ', hvalue
     
@@ -33,9 +33,24 @@ while True:
 	 hand2 = 'Queen'
        elif crown2 == 3:
 	 hand2 = 'King'
-    print 'Your hand consist of a %s and a %s.' % (hand1, hand2)
+    print 'Your hand consist of a/an %s and a/an %s.' % (hand1, hand2)
+    dealerh1 = random.randint (2,11)
+    dealerv = dealerh1
+    print 'The dealer has a value of: ', dealerv
+    if dealerv == 11:
+      dealerav = 1
+      dealerh1 = 'Ace'
+      print 'The dealer could also use a value of: 1'
+    if dealerh1 == 10:
+       crown3 = random.randint (1,3)
+       if crown3 == 1:
+         dealerh1 = 'Jack'
+       elif crown3 == 2:
+	 dealerh1 = 'Queen'
+       elif crown3 == 3:
+	 dealerh1 = 'King'
 
-
+    print 'The dealer holds a/an %s.' % (dealerh1)
 
   elif Begin == 'N' or Begin == 'n' or Begin == 'no' or Begin == 'No':
     break
